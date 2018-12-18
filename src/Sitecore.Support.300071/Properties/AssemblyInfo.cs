b9a46a2087c391702002bffd1f3155a1abf6465d
DK\psk <psk@sitecore.net>
2018-12-18 15:57:05 +0200

    Preparation based on default implementation

---
+++ src/Sitecore.Support.300071/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.300071")]
+[assembly: AssemblyProduct("Sitecore.Support.300071")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Memory leak in FilteringSiteProvider")]
+
---
+++ src/Sitecore.Support.300071/XA/Foundation/Multisite/Providers/BackendSiteProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.XA.Foundation.Multisite.Providers
+{
+  public class BackendSiteProvider : Sitecore.XA.Foundation.Multisite.Providers.BackendSiteProvider
+  {
+    public override Site GetSite(string siteName)
+    {
+      if (ParentProvider != null)
+      {
+        Site site = ParentProvider.GetSite(siteName);
+        if (site != null && IsMatch(site))
+        {
+          return site;
+        }
+      }
+      return null;
+    }
+
+    public override SiteCollection GetSites()
+    {
+      SiteCollection siteCollection = new SiteCollection();
+      if (ParentProvider != null)
+      {
+        siteCollection.AddRange(ParentProvider.GetSites().Where(IsMatch));
+      }
+      return siteCollection;
+    }
+  }
+}
