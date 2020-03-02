1353b87d07a5a3351cbda1af8a53f127209d5375
mao <mao@sitecore.net>
2020-03-02 17:16:00 +0200

    Add default implementation

---
+++ src/Sitecore.Support.300071/XA/Foundation/Multisite/Providers/BackendSiteProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.XA.Foundation.Multisite.Providers
+{
+    public class BackendSiteProvider : Sitecore.XA.Foundation.Multisite.Providers.BackendSiteProvider
+    {
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+        }
+
+        public override Site GetSite(string siteName)
+        {
+            Site site = ParentProvider?.GetSite(siteName);
+            if (site != null && IsMatch(site))
+            {
+                ModifySiteProperties(site);
+                return site;
+            }
+
+            return null;
+        }
+
+        public override SiteCollection GetSites()
+        {
+            SiteCollection siteCollection = new SiteCollection();
+            if (ParentProvider != null)
+            {
+                List<Site> list = ParentProvider.GetSites().Where(IsMatch).ToList();
+                list.ForEach(ModifySiteProperties);
+                siteCollection.AddRange(list);
+            }
+
+            return siteCollection;
+        }
+    }
+}
